0f6c60176c845db911801404c6aa2a3150d5126f
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-03-09 23:00:44 +0600

    banner design complete

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,39 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import ShowBanner from './showBanner';
-import Store from './Store';
+import React from 'react';
+import './Banner.css';
+import bannerImg from '../../images/portfolio-website-banner.png';
+
 
 const Banner = () => {
-    const [products, setProducts] = useState([]);
-    const [selectedProducts, setSelectedProducts] = useState([]);
-    const addedItem=[];
-    
-    const handleClick=(e)=>{
-        console.log(e);
-        addedItem.push(e);
-        setSelectedProducts(addedItem)
-    }
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, [])
     return (
         <div className='container'>
-            <div className='row'>
-                <div className='col col-md-6'>
-                <div className="row row-cols-1 row-cols-md-2 g-4">
-                    {
-                        products.map(product => <ShowBanner product={product} handleClick={handleClick} />)
-                    }
+            <div className='text-center'>
+                <p className='text-secondary mt-3'>Hello, I'm</p>
+                <h1>Fazle Rabby Talukder</h1>
+                <p className='mb-4'>Front-End Developer</p>
+                <a className='button1 me-2' href="https://drive.google.com/file/d/1KMw2QMgRhQdTTbLmcb9vRIEWSN-au0EN/view?usp=sharing" target="_blank" rel="noreferrer">Download CV</a>
+                <a className='button2' href="https://wa.me/8801739719796" target="_blank" rel="noreferrer">Let's Talk</a>
+            </div>
+            <div>
+                <div className="row mt-5">
+                    <div className="col-md-4 col-sm-12 position-relative">
+                        <div className='position-absolute bottom-0 start-0'>
+                            <a style={{ color: '#4FB4F2' }} href="https://www.linkedin.com/in/fazle-rabby-talukder/" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a> <br />
+                            <a style={{ color: '#4FB4F2' }} href="https://github.com/fazlerabbytalukder" target="_blank" rel="noreferrer"><i class="fa-brands fa-github"></i></a> <br />
+                            <a style={{ color: '#4FB4F2' }} href="https://www.facebook.com/frtfazlerabby/" target="_blank" rel="noreferrer"><i class="fa-brands fa-facebook"></i></a> <br />
+                            <div className="vl"></div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-12 text-center">
+                        <img className='img-fluid img-width' src={bannerImg} alt="" srcset="" />
+                    </div>
+                    <div className="col-md-4 col-sm-12 position-relative">
+                        <a className='vertical-button position-absolute bottom-0 end-0 fs-6' href="https://github.com/fazlerabbytalukder" target="_blank" rel="noreferrer">ScrollDown-</a>
+                    </div>
                 </div>
-                </div>
-                <div  className='col col-md-6'>
-                    {
-                        selectedProducts?.map(selectedProduct=><Store selectedProduct={selectedProduct}/>)
-                    }
-                </div>
-
             </div>
         </div>
     );
