15b7cb0b6250ce2c0277d48259017f3cef418bb2
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-03-10 11:42:12 +0600

    navbar complete

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    const [active, setActive] = useState('#')
+    return (
+        <div>
+            <nav>
+                <a href="#home" onClick={()=>setActive('#home')} className={active === '#home' ? 'active':''}><i class="fa-solid fa-house-chimney-user"></i></a>
+                <a href="#about" onClick={()=>setActive('#about')} className={active === '#about' ? 'active':''}><i class="fa-solid fa-user"></i></a>
+                <a href="#experience" onClick={()=>setActive('#experience')} className={active === '#experience' ? 'active':''}><i class="fa-solid fa-book-open-reader"></i></a>
+                <a href="#services" onClick={()=>setActive('#services')} className={active === '#services' ? 'active':''}><i class="fa-solid fa-handshake"></i></a>
+                <a href="#contact" onClick={()=>setActive('#contact')} className={active === '#contact' ? 'active':''}><i class="fa-solid fa-address-card"></i></a>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
